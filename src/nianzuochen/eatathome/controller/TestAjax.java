dc6526cf42921cf03eb99d5ce002c1665bbc89bd
ChengCuotuo <lei021191@outlook.com>
2019-05-17 21:13:30 +0800

    jackjson实现的json数据，由控制台向前台提交

--- src/nianzuochen/eatathome/controller/TestAjax.java
+++ src/nianzuochen/eatathome/controller/TestAjax.java
@@ -1,9 +1,11 @@
 package nianzuochen.eatathome.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,8 @@ public class TestAjax {
 	//响应前台对早餐的请求返回早餐的基本信息
 	@ResponseBody
 	@RequestMapping(value="easyui_pages/pages/zc1")
-	public String getInfo(HttpServletRequest request,String loginName) {
+	public void getInfo(HttpServletResponse response, String loginName) {
+		response.setContentType("text/html; charset=utf-8");
 		//单一处理，传递来的参数就是 zc1
 		Categoty categoty = cs.selectCategotyById("zc");
 		Map<String,String> map = new HashMap<String,String>();
@@ -41,6 +44,10 @@ public class TestAjax {
 		}
 	
 		System.out.println(map);
-		return json;
+		try {
+			response.getWriter().print(json);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
