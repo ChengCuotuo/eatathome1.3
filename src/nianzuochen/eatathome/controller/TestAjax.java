3be51869ff0d5dbd1168587bff81543757585c43
ChengCuotuo <lei021191@outlook.com>
2019-05-19 13:58:05 +0800

    页面标记

--- src/nianzuochen/eatathome/controller/TestAjax.java
+++ src/nianzuochen/eatathome/controller/TestAjax.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,23 +16,28 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import nianzuochen.eatathome.service.CategotyService;
+import nianzuochen.eatathome.service.MenuService;
 import nianzuochen.mybatis.domain.Categoty;
+import nianzuochen.mybatis.domain.Menu;
 
 @Controller
 public class TestAjax {
 	private CategotyService cs = new CategotyService();
-	
+	private MenuService ms = new MenuService();
 	//响应前台对早餐的请求返回早餐的基本信息
 	@ResponseBody
-	@RequestMapping(value="easyui_pages/pages/zc1")
-	public void getInfo(HttpServletResponse response) {
+	@RequestMapping(value="easyui_pages/pages/{formName}")
+	public void getInfo(HttpServletResponse response, @PathVariable String formName) {
 		response.setContentType("text/html; charset=utf-8");
 		//单一处理，传递来的参数就是 zc1
-		Categoty categoty = cs.selectCategotyById("zc");
+		Menu menu = ms.selectMenu(formName);
 		Map<String,String> map = new HashMap<String,String>();
-		if (categoty != null) {
-			map.put("id", categoty.getId());
-			map.put("name", categoty.getName());
+		if (menu != null) {
+			map.put("id", menu.getId());
+			map.put("name", menu.getName());
+			map.put("img", menu.getImg());
+			map.put("content", menu.getContent());
+			map.put("practice", menu.getPractices().toString());
 		}
 		//需要使用 jackjson 将数据(map类型数据)打包成 json 格式的数据流
 		String json = "";
@@ -45,6 +51,7 @@ public class TestAjax {
 	
 		System.out.println(map);
 		try {
+			//把 json 数据流传递给前台
 			response.getWriter().print(json);
 		} catch (IOException ex) {
 			ex.printStackTrace();
